79d14c79039f712982c57d385577abc8ed9a496c
268gopal <162484336+268gopal@users.noreply.github.com>
2024-03-07 07:01:09 +0000

    changes

---
+++ basics/datatypes.js
@@ -0,0 +1,4 @@
+let name="Gopal"
+let age =21
+let isLoggedIn= false
+console.table([name,age,isLoggedIn]);
---
+++ basics/variable.js
@@ -0,0 +1,13 @@
+const accountId = 1
+let accountEmail ="gogo@go.com"
+var accountPassword = 123
+accountCity ="bhubneshwar"
+
+console.log(accountId);
+accountEmail ="abc@abc.com"
+accountPassword = 12121
+accountCity = "jamshedpur"
+
+let accountState
+
+console.table([accountEmail,accountId,accountPassword,accountCity ,accountState])
